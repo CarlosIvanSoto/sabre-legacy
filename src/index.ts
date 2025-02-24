cdcf21c5bea5399b5e2df28a2c218227fe2c5110
Carlos Ivan Soto Perez <gameue@hotmail.com>
2025-02-24 10:42:44 -0600

    export type Sabre options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Sabre as SabreLegacy } from './sabre';
 export { ErrorInResponse } from './common/utils/error-in-response'
 export { FaultError } from './common/utils/fault-error'
+export * from './interfaces'
 export * from './daily-sales/interfaces/daily-sales-report-options.interface'
 export * from './daily-sales/interfaces/daily-sales-summary-options.interface'
 export * from './queue/interfaces/queue-access-options.interface'
