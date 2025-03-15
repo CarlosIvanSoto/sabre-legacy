f820d7370cdb0c997b4550833eb435c8c103988b
Carlos Ivan Soto Perez <gameue@hotmail.com>
2025-03-15 13:49:28 -0600

    Add: Se agrego el enlace de documentación de las funciones de Authentication

--- src/authentication/authentication..ts
+++ src/authentication/authentication..ts
@@ -5,21 +5,40 @@ import { pingRequest, sessionCloseRequest, sessionCreateRequest, tokenCreateRequ
 export class Authentication {
   constructor(private readonly sabre: Sabre) {}
 
+  /**
+   * See https://developer.sabre.com/docs/soap_apis/session_management/create_session
+   * @returns string
+   */
   async sessionCreate(): Promise<string> {
     this.sabre.setAction(ActionsRQ.SESSION_CREATE);
     const xml = await this.sabre.auth(sessionCreateRequest);
     return xml
   }
+
+  /**
+   * See https://developer.sabre.com/docs/soap_apis/session_management/close_session
+   * @returns string
+   */
   async sessionClose(): Promise<string> {
     this.sabre.setAction(ActionsRQ.SESSION_CLOSE);
     const xml = await this.sabre.post(sessionCloseRequest);
     return xml
   }
+
+  /**
+   * See https://developer.sabre.com/docs/soap_apis/session_management/refresh_session
+   * @returns string
+   */
   async ping(): Promise<string> {
     this.sabre.setAction(ActionsRQ.PING);
     const xml = await this.sabre.post(pingRequest);
     return xml
   }
+
+  /**
+   * See https://developer.sabre.com/docs/soap_apis/session_management/create_access_token
+   * @returns string
+   */
   async tokenCreate(): Promise<string> {
     this.sabre.setAction(ActionsRQ.TOKEN_CREATE);
     const xml = await this.sabre.auth(tokenCreateRequest);
