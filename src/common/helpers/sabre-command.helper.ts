90f720c13be7705cc88176be1ae53cda090c5e58
Marcelo Guzman <rmguzmand@aeromexicofob.com>
2025-03-25 19:17:44 -0600

    Ajuste para que compilara.
    Ajuste para mandar el body request en una linea.

--- src/common/helpers/sabre-command.helper.ts
+++ src/common/helpers/sabre-command.helper.ts
@@ -3,11 +3,7 @@
  * @returns 
  */
 function sabreCommand (command: string): string {
-    return `<SabreCommandLLSRQ xmlns="http://webservices.sabre.com/sabreXML/2011/10" ReturnHostCommand="true" Version="2.0.0">
-        <Request Output="SCREEN">
-            <HostCommand>${command}</HostCommand>
-        </Request>
-    </SabreCommandLLSRQ>`;
+    return `<SabreCommandLLSRQ xmlns="http://webservices.sabre.com/sabreXML/2011/10" ReturnHostCommand="true" Version="2.0.0"><Request Output="SCREEN"><HostCommand>${command}</HostCommand></Request></SabreCommandLLSRQ>`;
   }
   
   export { sabreCommand }
--- src/currency/currency.ts
+++ src/currency/currency.ts
@@ -1,6 +1,6 @@
 import { ActionsRQ } from "../common/interfaces/actions.interface";
 import { SabreCommandLLSRS } from "../common/interfaces/sabre-command.interface";
-import { parseXMLToSabreCommands } from "../common/utils/parse-xml-to-sabre-command";
+import { parseXMLToSabreCommand } from "../common/utils/parse-xml-to-sabre-command";
 import { Sabre } from "../sabre";
 import { currencyRequest } from "./requests/currency.request";
 import { CurrencyOptions } from "./requests/interfaces/currency-options";
@@ -16,6 +16,6 @@ export class Currency {
     this.sabre.setAction(ActionsRQ.SABRE_COMMANDS);
     const {from, to, date} = payload;
     const xml = await this.sabre.post((headersPayload) => currencyRequest({ from, to, date, ...headersPayload }))
-    return parseXMLToSabreCommands(xml)
+    return parseXMLToSabreCommand(xml)
   }
 }
